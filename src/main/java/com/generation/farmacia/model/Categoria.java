6806956cd6fde8c510876d82acfec3e6ae13016d
Luiz Pascucho <luizpascucho@bol.com.br>
2024-04-12 11:44:24 -0300

    criando CRUD de categoria

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,52 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotBlank(message = "O nome é obrigatorio")
+	@Size(min = 3, max = 40, message = "O atributo nome de conter no minimo 3 e no maximo 40 caracteres ")
+	private String nome;
+	
+	@NotBlank(message = "A descrição é obrigatorio")
+	@Size(min = 10, max = 1000, message = "O atributo descrição de conter no minimo 10 e no maximo 1000 caracteres ")
+	private String descricao;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
